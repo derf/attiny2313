218115070f158764c1280e15e139fe8510e33f45
Daniel Friesel <derf@finalrewind.org>
2011-05-26 18:41:53 +0200

    Blinkenlights v1

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define BRIGHTNESS_MAX 12
+#define BRIGHTNESS_MAX 30
 
-void set_led_state(char led1, char led2)
+inline void set_led_states(char led1, char led2)
 {
 	PORTD = (led1 << PD5) | (led2 << PD6);
 }
@@ -25,29 +25,37 @@ int main (void)
 	char key2 = 0;
 	char key3 = 0;
 
-	int brightness = 0;
+	int brightness = 1;
+	int direction = 1;
+
+	int cnt = 0;
+	int cur = 0;
 
 	DDRD = (1 << PD5) | (1 << PD6) ;
-	PORTD = (1 << PD5) | (1 << PD6);
 
 	while (1) {
-		key1 = check_key_edge(key1, PD2);
-		key2 = check_key_edge(key2, PD3);
-		key3 = check_key_edge(key3, PD4);
 
-		if ((key1 == 1) && (brightness > 0))
-			brightness--;
-		else if ((key2 == 1) && (brightness < BRIGHTNESS_MAX))
-			brightness++;
+		cnt++;
 
-		if (brightness != BRIGHTNESS_MAX) {
-			_delay_us(brightness * 100);
-			set_led_state(0, 1);
+		if (cnt > 100) {
+			brightness += direction;
+
+			if (brightness == BRIGHTNESS_MAX)
+				direction = -1;
+			else if (brightness == 0)
+				direction = 1;
+
+			cnt = 0;
 		}
-		if (brightness != 0) {
-			_delay_us((BRIGHTNESS_MAX - brightness) * 100);
-			set_led_state(1, 0);
-		}
+
+		cur = cnt % BRIGHTNESS_MAX;
+
+		if (!cur)
+			set_led_states(1, 0);
+
+		if (cur == brightness)
+			set_led_states(0, 1);
+
 	}
 
 	return 0;
