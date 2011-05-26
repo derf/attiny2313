a37829d1f0858f37344279e35b757179dbed6137
Daniel Friesel <derf@finalrewind.org>
2011-05-26 13:20:13 +0200

    I can haz input

--- main.c
+++ main.c
@@ -1,13 +1,48 @@
 #include <avr/io.h>
 
+inline void set_led_state(char led1, char led2)
+{
+	PORTD = (led1 << PD5) | (led2 << PD6);
+}
+
+int check_key_edge(int key, int port)
+{
+	int ret;
+
+	if (PIND & (1 << port)) {
+		if (!key)
+			ret = 1;
+		else
+			ret = 2;
+	} else
+		ret = 0;
+
+	return ret;
+}
+
 int main (void) {
 
-	DDRD = (1 << 5);
-	PORTD = (1 << PD5);
+	char led1 = 0;
+	char led2 = 0;
+	char key1 = 0;
+	char key2 = 0;
+	char key3 = 0;
+
+	DDRD = (1 << PD5) | (1 << PD6) ;
+	PORTD = (1 << PD5) | (1 << PD6);
 
 	while (1) {
-		PORTD = 0x00;
-		PORTD = (1 << PD5);
+		key1 = check_key_edge(key1, PD2);
+		key2 = check_key_edge(key2, PD3);
+		key3 = check_key_edge(key3, PD4);
+
+		if (key1 == 1)
+			led1 = !led1;
+
+		if (key2 == 1)
+			led2 = !led2;
+
+		set_led_state(led1, led2);
 	}
 
 	return 0;
