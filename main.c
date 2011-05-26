fe0534d8c8a0b54b2e88a61ef4bbb5e12e7a6307
Daniel Friesel <derf@finalrewind.org>
2011-05-26 18:51:59 +0200

    Blinkenlights v2

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <util/delay.h>
 
 #define BRIGHTNESS_MAX 30
+#define CNTT_MAX 300
+#define CNTT_MIN 30
+#define CNTT_INT 25
 
 inline void set_led_states(char led1, char led2)
 {
@@ -29,15 +32,24 @@ int main (void)
 	int direction = 1;
 
 	int cnt = 0;
+	int cnt_max = 100;
 	int cur = 0;
 
 	DDRD = (1 << PD5) | (1 << PD6) ;
 
 	while (1) {
 
+		key1 = check_key_edge(key1, PD3);
+		key2 = check_key_edge(key2, PD4);
+
 		cnt++;
 
-		if (cnt > 100) {
+		if ((key1 == 1) && (cnt_max > CNTT_MIN))
+			cnt_max -= CNTT_INT;
+		else if ((key2 == 1) && (cnt_max < CNTT_MAX))
+			cnt_max += CNTT_INT;
+
+		if (cnt > cnt_max) {
 			brightness += direction;
 
 			if (brightness == BRIGHTNESS_MAX)
