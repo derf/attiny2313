6d43105dda7d4dadf0f4ef0421cd5a553b2b8de5
Daniel Friesel <derf@finalrewind.org>
2012-02-09 11:05:58 +0100

    rgb: less fade steps (-> less flickering)

--- attiny2313/rgb.c
+++ attiny2313/rgb.c
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define BRIGHTNESS_MAX 80
+#define BRIGHTNESS_MAX 40
 
 inline void set_led(char red, char green, char blue)
 {
@@ -14,8 +14,8 @@ inline void set_led(char red, char green, char blue)
 int main (void)
 {
 	char red, green, blue;
-	char b_red, b_green, b_blue;
-	char n_red, n_green, n_blue;
+	unsigned int b_red, b_green, b_blue;
+	unsigned int n_red, n_green, n_blue;
 
 	unsigned int step = 0;
 
@@ -23,12 +23,14 @@ int main (void)
 	unsigned char cur = 0;
 
 	unsigned char pwm[16] = {
-		0, 1, 1, 1, 1, 3, 4, 5, 8, 11, 15, 21, 30, 43, 60, 80
+		0, 1, 1, 1, 1, 2, 2, 3, 4, 6, 8, 11, 15, 22, 30, 40
 	};
 
 	unsigned int cnt_max = BRIGHTNESS_MAX;
 
 	DDRD = (1 << PD4) | (1 << PD5) | (1 << PD6);
+
+	b_red = b_green = b_blue = n_red = n_green = n_blue = 20;
 
 	while (1) {
 
@@ -44,10 +46,10 @@ int main (void)
 				n_green = pwm[rand() % 16];
 				n_blue = pwm[rand() % 16];
 			}
-			if (!(step % 12)) {
-				b_red = ((unsigned int)b_red * 19 + n_red) / 20;
-				b_green = ((unsigned int)b_green * 19 + n_green) / 20;
-				b_blue = ((unsigned int)b_blue * 19 + n_blue) / 20;
+			if (!(step % 30)) {
+				b_red = (b_red * 3 + n_red) / 4;
+				b_green = (b_green * 3 + n_green) / 4;
+				b_blue = (b_blue * 3+ n_blue) / 4;
 			}
 		}
 
